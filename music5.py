3ab5350002d83f6321299b0b033c5914fad1d741
Cameron <cameronadevereaux@gmail.com>
2023-04-06 15:56:01 -0700

    Added wav output

--- music5.py
+++ music5.py
@@ -1,6 +1,11 @@
 import subprocess
 from sys import argv
+from scipy.io.wavfile import write
+import numpy as np
 
 subprocess.run("python pass1.py " + argv[1])
 subprocess.run("python pass2.py pass1.out")
 subprocess.run("python pass3.py pass2.out")
+
+samples = np.fromfile("audio.raw", np.float32)
+write("output.wav", 44100, samples)
